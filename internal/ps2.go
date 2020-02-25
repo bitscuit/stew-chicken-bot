a0f4464ef8bbf6b74bb6f24eb8940ea7c9022b92
Henry Li <zbhenryli@gmail.com>
2020-02-24 23:45:20 -0500

    Ps2 alerts (#4)
    * can now get active ps2 alerts
    
    * ps2 alerts looks within the past hour
    
    * added minutes duration since alert started

--- internal/ps2.go
+++ internal/ps2.go
@@ -6,7 +6,9 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
 	"strings"
+	"time"
 )
 
 type Players struct {
@@ -67,6 +69,79 @@ func getCerts(args string) (string, error) {
 	return args + " has " + body.Player[0].Cert.AvailablePoints + " certs", nil
 }
 
+type WorldEvents struct {
+	Events []Event `json:"world_event_list"`
+}
+
+type Event struct {
+	MetagameEventID    string   `json:"metagame_event_id"`
+	Timestamp          string   `json:"timestamp"`
+	MetagameEventState string   `json:"metagame_event_state_name"`
+	MetagameEventType  struct { // only alerts will have this
+		Name struct {
+			English string `json:"en"`
+		} `json:"name"`
+	} `json:"metagame_event_id_join_metagame_event"`
+}
+
 func isAlert() (string, error) {
-	return "WIP to get alerts", nil
+	searchTime := time.Now().Unix() - 3600
+	baseUrl := "http://census.daybreakgames.com"
+	path := "/get/ps2:v2/world_event"
+	search := "?type=METAGAME&world_id=17&after=" + strconv.FormatInt(searchTime, 10) + "&c:limit=50&c:join=metagame_event^terms:description.en=*lock"
+	url := baseUrl + path + search
+	fmt.Println(url)
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return "", errors.New("Failed request")
+	}
+
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", errors.New("Failed do")
+	}
+	defer resp.Body.Close()
+
+	var body WorldEvents
+	if err := json.NewDecoder(resp.Body).Decode(&body); err != nil {
+		return "", errors.New("Failed to decode json")
+	}
+
+	fmt.Println(body)
+
+	count := 0
+	result := make(map[string]Event)
+	// loop through Events to remove non-alerts and ended alerts
+	for i := len(body.Events) - 1; i > -1; i-- {
+		if body.Events[i].MetagameEventType.Name.English == "" { // might be a better way to do this
+			continue
+		}
+		if body.Events[i].MetagameEventState == "ended" {
+			v, ok := result[body.Events[i].MetagameEventID]
+			if ok { // remove matching start alert
+				delete(result, v.MetagameEventID)
+			}
+			delete(result, body.Events[i].MetagameEventID)
+		} else {
+			result[body.Events[i].MetagameEventID] = body.Events[i]
+		}
+		count++
+	}
+	fmt.Println(result)
+	fmt.Println(count)
+	alerts := ""
+	for _, v := range result {
+		ts, err := strconv.ParseInt(v.Timestamp, 10, 64)
+		if err != nil {
+			return "", errors.New("Something went horribly wrong")
+		}
+		duration := int(time.Since(time.Unix(ts, -1)).Minutes())
+		alerts += v.MetagameEventType.Name.English + " started " + strconv.Itoa(duration) + " minutes go\n"
+	}
+	if alerts == "" {
+		alerts = "No alerts"
+	}
+	return alerts, nil
 }
