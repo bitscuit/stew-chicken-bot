43f5ad7992a3841a0477198f81e9b4c55430d80c
bitscuit <zbhenryli@gmail.com>
2020-02-28 22:51:58 -0500

    use table tests for isAlert

--- internal/ps2_test.go
+++ internal/ps2_test.go
@@ -16,26 +16,38 @@ func init() {
 }
 
 func TestIsAlert(t *testing.T) {
-	path := filepath.Join("testdata", "ps2", "one-alert.json")
-	json, err := ioutil.ReadFile(path)
-	if err != nil {
-		t.Error("Failed to get json file")
+	tests := []struct {
+		in  string
+		out string
+	}{
+		{"one-alert.json", "154: Hossin Enlightenment started"},
+		{"no-alerts.json", "No alerts"},
 	}
 
-	mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
-		r := ioutil.NopCloser(bytes.NewReader([]byte(json)))
-		return &http.Response{
-			StatusCode: 200,
-			Body:       r,
-		}, nil
-	}
-	got, err := isAlert("1582856026")
-	if err != nil {
-		t.Error(err)
+	for _, test := range tests {
+		path := filepath.Join("testdata", "ps2", test.in)
+		json, err := ioutil.ReadFile(path)
+		if err != nil {
+			t.Error("Failed to get json file")
+		}
+
+		mocks.GetDoFunc = func(*http.Request) (*http.Response, error) {
+			r := ioutil.NopCloser(bytes.NewReader([]byte(json)))
+			return &http.Response{
+				StatusCode: 200,
+				Body:       r,
+			}, nil
+		}
+
+		got, err := isAlert("1582856026")
+		if err != nil {
+			t.Error(err)
+		}
+
+		wanted := test.out
+		if !strings.Contains(got, wanted) {
+			t.Error("Expected: \"" + wanted + "\", but got: \"" + got + "\"")
+		}
 	}
 
-	wanted := "154: Hossin Enlightenment started"
-	if !strings.Contains(got, wanted) {
-		t.Error("Expected: \"" + wanted + "\", but got: \"" + got + "\"")
-	}
 }
